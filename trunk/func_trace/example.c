63a61c19e421f2d81452cfa1521ca27a67ee2617
bigwhite.cn <bigwhite.cn@e8210f93-3b20-0410-8e6a-87f0a448f9ba>
2011-07-13 08:59:12 +0000

    modify example.c

--- trunk/func_trace/example.c
+++ trunk/func_trace/example.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
-int main(int argc, const char *argv[]);
-
-struct addr_name_map_t {
-    unsigned int addr;
-    char buf[64];
-};
-
-
 static void foo2() {
+
 }
 
 void foo1() {
@@ -16,23 +9,12 @@ void foo1() {
 }
 
 void foo(){
-    chdir("/export/home/baim");
+    chdir("/home/tonybai");
     foo1();
 }
 
-struct addr_name_map_t maps[20] = {
-    {(unsigned int)&main, "main"},
-    {(unsigned int)&foo, "foo"},
-    {(unsigned int)&foo1, "foo1"},
-    {(unsigned int)&foo2, "foo2"}
-};
-
 int main(int argc, const char *argv[])
 {
-//    printf("main addr = %p\n", main);
-//    printf("foo addr = %p\n", foo);
-//    printf("foo1 addr = %p\n", foo1);
-//    printf("foo2 addr = %p\n", foo2);
     foo();    
     return 0;
 }
