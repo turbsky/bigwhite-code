3aae4cfc75a42756fe21c689386e2aac663859f8
bigwhite.cn <bigwhite.cn@e8210f93-3b20-0410-8e6a-87f0a448f9ba>
2011-07-13 08:40:01 +0000

    add func_trace

---
+++ func_trace/func_trace.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h> /* for PATH_MAX */
+
+static char path[PATH_MAX];
+
+__attribute__((constructor))
+static void executable_path_init() {
+    char    buf[PATH_MAX];
+
+    memset(buf, 0, sizeof(buf));
+    memset(path, 0, sizeof(path));
+
+#ifdef _SOLARIS_TRACE
+    getcwd(buf, PATH_MAX);
+    sprintf(path, "%s/%s", buf, getexecname());
+#elif _LINUX_TRACE
+    readlink("/proc/self/exe", path, PATH_MAX);
+#else
+    #error "The OS has not been supported!"
+#endif
+}
+
+void __cyg_profile_func_enter(void *this_fn, void *call_site) {
+    char buf[PATH_MAX];
+    char cmd[PATH_MAX];
+
+    memset(buf, 0, sizeof(buf));
+    memset(cmd, 0, sizeof(cmd));
+
+    sprintf(cmd, "addr2line 0x%x -e %s -f|head -1", (unsigned int)this_fn, path);
+
+    FILE *ptr = NULL;
+    memset(buf, 0, sizeof(buf));
+
+    if ((ptr = popen(cmd, "r")) != NULL) {
+        fgets(buf, PATH_MAX, ptr);
+        printf("enter func %s", buf);
+    }
+
+    (void) pclose(ptr);
+}
+
+void __cyg_profile_func_exit(void *this_fn, void *call_site) {
+    char buf[PATH_MAX];
+    char cmd[PATH_MAX];
+
+    memset(buf, 0, sizeof(buf));
+    memset(cmd, 0, sizeof(cmd));
+
+    sprintf(cmd, "addr2line 0x%x -e %s -f|head -1", (unsigned int)this_fn, path);
+
+    FILE *ptr = NULL;
+    memset(buf, 0, sizeof(buf));
+
+    if ((ptr = popen(cmd, "r")) != NULL) {
+        fgets(buf, PATH_MAX, ptr);
+        printf("exit func %s", buf);
+    }
+
+    (void) pclose(ptr);
+}
