d2d95f49f56eda015c8cf8338c13cb9ccefbc34f
bigwhite.cn <bigwhite.cn@e8210f93-3b20-0410-8e6a-87f0a448f9ba>
2013-03-18 07:58:15 +0000

    modify some code format and comments

--- trunk/sys-running-data-extract/producer.c
+++ trunk/sys-running-data-extract/producer.c
@@ -31,7 +31,8 @@ main()
 
     errno = 0;
     char *p = NULL;
-    p = mmap(NULL, size, PROT_WRITE|PROT_READ, MAP_SHARED, fileno(fp), 0);
+    p = mmap(NULL, size, PROT_WRITE|PROT_READ, 
+             MAP_SHARED, fileno(fp), 0);
     if (p == MAP_FAILED) {
         printf("can not mmap file, error = %d\n", errno);
         fclose(fp);
@@ -44,7 +45,7 @@ main()
         return -1;
     }
 
-    /* round up to 8 */
+    /* round up to a multiple of 8 */
     while((int)p % 8 != 0) {
         p++;
     }
